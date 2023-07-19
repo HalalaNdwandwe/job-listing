c64b8caa20d458d92c20a75393b4aab91ceca16f
Halala Ndwandwe <hphakamile@gmail.com>
2023-07-19 11:47:26 +0200

    updated addjob page

--- src/Components/AddJob.jsx
+++ src/Components/AddJob.jsx
@@ -4,17 +4,17 @@ import TopBar from "./TopBar";
 
 const AddJobs =()=> {
     return(
-        <section className="bg-pjImg  bg-center bg-cover">
+        <section className="bg-pjImg  bg-center justify-center m-10 bg-cover">
             <div className="container mx-auto ">
                 <form className="border-2 border-zinc-600 rounded py-10 px-7 ml-40 mr-40">
                     <h2 className="font-bold text-xl flex justify-center">Post Job</h2>
                     <div className="mt-3">
-                        <input type="text" name="tittle" placeholder="Job tittle*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
-                        <input type="text" name="location" placeholder="Location*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
-                        <input type="text" name="description" placeholder="Description*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
-                        <input type="text" name="name" placeholder="Company Name*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
-                        <input type="text" name="requirements" placeholder="Requirements*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
-                        <input type="text" name="skills" placeholder="Skills*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"/>
+                        <input type="text" name="tittle" placeholder="Job tittle*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
+                        <input type="text" name="location" placeholder="Location*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
+                        <input type="text" name="description" placeholder="Description*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
+                        <input type="text" name="name" placeholder="Company Name*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
+                        <input type="text" name="requirements" placeholder="Requirements*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
+                        <input type="text" name="skills" placeholder="Skills*" className="block w-full border-2 bg-gray-100 p-2 rounded-2xl"required/>
                     </div>
                     <button type="submit" className="btn bg-gray-700 text-white text-xl mt-8 px-4 py-2 rounded-2xl flex justify-end" onClick={<Jobs/>}>Post</button>
 
